42fa603db77ddf9450b60fa137c8a047533bcd82
Tracy Rotton <trotton@rp3agency.com>
2014-10-05 11:48:29 -0400

    Adding "gulp-combine-media-queries" (and making sure the output is ignored by git)

--- demo/gulpfile.js
+++ demo/gulpfile.js
@@ -2,10 +2,13 @@ var gulp			= require('gulp'),
 	gutil			= require('gulp-util'),
 	compass			= require('gulp-compass'),
 	watch			= require('gulp-watch'),
-	autoprefixer	= require('gulp-autoprefixer');
+	autoprefixer	= require('gulp-autoprefixer'),
+	cmq				= require('gulp-combine-media-queries'),
+	del				= require('del'),
+	rename			= require('gulp-rename');
 
 
-gulp.task( 'compass', function() {
+gulp.task( 'styles', function() {
 	gulp.src('scss/**/*.scss')
 		.pipe(compass({
 			config_file: './config.rb',
@@ -17,10 +20,24 @@ gulp.task( 'compass', function() {
 		.pipe(gulp.dest('css'));
 });
 
-gulp.task( 'default', ['compass'] );
+gulp.task( 'cmq', function() {
+	gulp.src('css/main.css')
+		.pipe( cmq( { log: true } ) )
+		.pipe( rename( 'css/main.cmq.css' ) )
+		.pipe( gulp.dest( '.' ) );
+});
 
-gulp.task( 'watch', function() {
-	gulp.watch('scss/**/*.scss', function() {
-		gulp.run('compass');
+gulp.task( 'clean', function() {
+	del(['css/main*.css'], function (err) {
+		console.log('Files deleted');
 	});
+});
+
+gulp.task( 'default', ['styles'] );
+
+gulp.task('watch', function() {
+	gulp.start('default');
+
+	// Watch .scss files
+	gulp.watch('scss/*.scss', ['styles']);
 });
